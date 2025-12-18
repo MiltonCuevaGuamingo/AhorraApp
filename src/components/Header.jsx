97a3d2443c9d0e1c0ab4286bc57228e4ad0b3748
Andrescgtw <caguanoluisa@espe.edu.ec>
2025-12-18 01:03:38 -0500

    Agrego <h> para centrar el titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,9 @@ import ChangeBack from './ChangeBack';
 function Header() {
   return (
     <header className="header">
+      <h1> </h1>
       <h1 className="header-title">Cotizador de prestamos</h1>
-      <ChangeBack />
+      <ChangeBack></ChangeBack>
     </header>
   );
 }
